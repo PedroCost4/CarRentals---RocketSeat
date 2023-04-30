23e6657b72fa6e5304145925ea6db23e91305ef9
Pedro Costa <pedro.costacdc@gmail.com>
2023-04-30 18:12:33 -0300

    feat:prisma add

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { router } from './routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 
+
 const app = express();
 
 app.use(express.json());
@@ -10,8 +11,6 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
-
-
 
 const PORT = process.env.PORT || 3333;
 
